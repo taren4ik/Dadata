ce100e8d6619f07f5c9bc54234f9419f585f42f2
Dmitry Permyakov <taren4ik@mail.ru>
2026-09-01 12:09:36 +1000

    change dadata dag

--- dag_dadata.py
+++ dag_dadata.py
@@ -4,7 +4,7 @@ import os
 from datetime import datetime
 
 from airflow.decorators import dag, task
-from airflow.providers.postgres.hooks.postgres import PostgresHook
+#from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 from psycopg2.extras import execute_values
 
